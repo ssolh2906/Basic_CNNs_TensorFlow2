d9e37226e02027032b426552849ca35139b6aa3a
Solhee Park <solheepark29@gmail.com>
2021-12-23 14:08:56 +0900

    data cleansing

--- cleansing_dataset.py
+++ cleansing_dataset.py
@@ -1,7 +1,11 @@
 import os
-import random
 import shutil
+import sys
 from configuration import TRAIN_SET_RATIO, TEST_SET_RATIO
+from PIL import Image
+
+global target_size_x
+global target_size_y
 
 # Split Dataset 3
 # Start~End frame 들을 한 세트로 움직임
@@ -11,16 +15,19 @@ class SplitDataset():
                  show_progress=False):
         self.dataset_dir = dataset_dir  # original data set
         self.saved_dataset_dir = saved_dataset_dir  # data set(result folder)
-
-        self.train_file_path = []
-        self.valid_file_path = []
-        self.test_file_path = []
+        self.saved_train_dir = saved_dataset_dir + "/train/"
+        self.saved_valid_dir = saved_dataset_dir + "/valid/"
+        self.saved_test_dir = saved_dataset_dir + "/test/"
+        self.exception_dir = dataset_dir + "/size_exception/"
         # [self.index_label_dict[index][class], self.index_label_dict[index][subclass], list((경로,[S01.jpg,M01.jpg]),(경로2,[S01,M01,E01] ),...)]
 
         self.index_label_dict = {}  # class 이름을 value로 갖는 dict {0:class, 1:B001, 2:B002, class2, ...}
         # >>이중 dict {0:{"class":, "subclass":}, 1: {"class":, "subclass":}.... }
 
         self.show_progress = show_progress
+
+        if not os.path.exists(self.exception_dir):
+            os.mkdir(self.exception_dir)
 
         self.json_missing = []
 
@@ -41,7 +48,6 @@ class SplitDataset():
                                              item)  # os.path.join :  두인자를 하나로 합쳐 1개 경로로 만듬, 경로 + 파일명 >> join(root, class, item)
                     class_subclass = item_path[item_path.find(self.dataset_dir) + len(self.dataset_dir) + 1:]
                     class_name = os.path.split(os.path.split(item_path)[0])[1]
-                    #class_name = class_subclass[:class_subclass.find("/")]
                     if os.path.isdir(item_path):  # 디렉토리면
                         label_names.append({"class": class_name, "subclass": item})  # label name 리스트에 추가 <label name
         return label_names  # class 명을 저장하는 1차원배열, [class1, B001, B002, class2, ...  ] >> list of dictionary [{"class":, "item": },{"class":, "item":}, ....]
@@ -74,7 +80,35 @@ class SplitDataset():
             all_file_path.append(file_path)
         return all_file_path  # list of list , 행 : class/subclass 열 : filename
 
+    def start_cleansing(self):
+        all_file_paths = self.__get_all_file_path()
 
+        for action in all_file_paths:  # action : dict
+            for key, file_list in action.items():
+                for file in file_list:
+                    if 'jpg' in file:
+                        full_file_path = key+file
+                        image = Image.open(full_file_path)
+
+                        print(image.size)
+                        if image.size[0] != target_size_x or image.size[1] != target_size_y :
+                            dir_lv3 = os.path.split(full_file_path)
+                            dir_lv2 = os.path.split(dir_lv3[0])
+                            dir_lv1 = os.path.split(dir_lv2[0])
+
+                            if self.show_progress:
+                                print("[Image size exception] :", full_file_path)
+
+                            for other_file in file_list:
+                                #shutil.move(key+other_file, key+other_file)
+                                dst_path = os.path.join(self.exception_dir, dir_lv1[1])
+                                dst_path = os.path.join(dst_path, dir_lv2[1])
+                                file_name = os.path.split(key + other_file)[1]
+                                if not os.path.exists(dst_path):
+                                    os.makedirs(dst_path, exist_ok = True)
+                                shutil.move(key+other_file, os.path.join(dst_path, file_name))
+                                print("move :", key+other_file, "to..",os.path.join(dst_path,file_name))
+                            break
 
 
 
@@ -99,8 +133,15 @@ class SplitDataset():
 
 
 
+
+
+
+
+
 if __name__ == '__main__':
+    target_size_x = sys.argv[1]
+    target_size_y = sys.argv[2]
     split_dataset = SplitDataset(dataset_dir="original_dataset",
                                  saved_dataset_dir="dataset",
                                  show_progress=True)
-    split_dataset.start_comparing()
+    split_dataset.start_cleansing()
